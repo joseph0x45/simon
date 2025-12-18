06913195fb464da83e7f6929b096c7afdaca1482
joseph0x45 <zozozozeph@gmail.com>
2025-12-18 11:48:09 +0000

    breh

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+	token := os.Getenv("TOKEN")
+	session, err := discordgo.New("Bot " + token)
+	if err != nil {
+		panic(err)
+	}
+	session.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+		currentChannel, err := s.Channel(m.ChannelID)
+		if err != nil {
+			log.Println("[ERROR]: Failed to get channel with ID:", err.Error())
+			return
+		}
+		if currentChannel.Name != "bot" {
+			if strings.HasPrefix(m.Content, "m!play ") {
+				if err := s.ChannelMessageDelete(m.ChannelID, m.ID); err != nil {
+					log.Println("[ERROR]: Failed to delete message with ID:", err.Error())
+				}
+				mention := fmt.Sprintf("%s", m.Author.Mention())
+				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Hey %s, N'envoie pas les messages la ici singe", mention))
+			}
+		}
+	})
+
+	session.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = session.Open()
+	if err != nil {
+		panic(err)
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /message", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+	defer session.Close()
+	log.Println("Bot online")
+	log.Println("Starting HTTP server")
+	if err := http.ListenAndServe(":6969", mux); err != nil {
+		panic(err)
+	}
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGTERM, syscall.SIGINT, os.Interrupt)
+	<-sc
+
+}
